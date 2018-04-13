3306e48e53d476f2fd7ee3736ec2e45eb8b98e17
Luis Tejero <luisfaridta@gmail.com>
2018-04-13 11:42:31 +0100

    third function passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,7 +1,14 @@
+#1
 def pet_shop_name(shopname)
   return shopname[:name]
 end
 
+#2
 def total_cash(cashInShop)
   return cashInShop[:admin][:total_cash]
+end
+
+#3
+def add_or_remove_cash(shop, amount)
+  shop[:admin][:total_cash] += amount.to_i 
 end

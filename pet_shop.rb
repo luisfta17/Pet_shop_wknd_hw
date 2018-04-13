60901b6f969fe926fc6ba7acd9d41a3a3cfcb7f2
Luis Tejero <luisfaridta@gmail.com>
2018-04-13 11:51:47 +0100

    5th function passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -8,7 +8,12 @@ def total_cash(cashInShop)
   return cashInShop[:admin][:total_cash]
 end
 
-#3
+#3,4
 def add_or_remove_cash(shop, amount)
-  shop[:admin][:total_cash] += amount.to_i 
+  shop[:admin][:total_cash] += amount.to_i
+end
+
+#5
+def pets_sold(shopname)
+  return shopname[:admin][:pets_sold]
 end
